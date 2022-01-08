7f06b4f8c136d86dae4ebc027775c36b514132d0
Olivier Gamache <olivier.gamache@.elapsetech.com>
2022-01-08 12:19:02 -0500

    👀 now also beau-t on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import type { Component } from 'solid-js';
 import planetA from './assets/planetA.gif';
 import planetB from './assets/planetB.gif';
 import planetC from './assets/planetC.gif';
+import runner from './assets/runner.gif';
 import astro from './assets/astro.png';
 
 import phone from './assets/phone.png';
 
 import gameplay from './assets/gameplay.mp4';
+import gameplay_still from './assets/gameplay-still.png';
 
 import title from './assets/title.png';
 import gplay from './assets/gplay.png';
@@ -19,9 +21,12 @@ const App: Component = () => {
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={title} class={styles.title} alt="logo" />
-        <img src={planetA} class={styles.planetA} alt="earth" />
-        <img src={planetB} class={styles.planetB} alt="planet" />
-        <img src={planetC} class={styles.planetC} alt="moon" />
+        <div class={styles.galaxy}>
+          <img src={planetA} class={styles.planetA} alt="earth" />
+          <img src={planetB} class={styles.planetB} alt="planet" />
+          <img src={planetC} class={styles.planetC} alt="moon" />
+        </div>
+        <img src={runner} class={styles.runner} alt="runner" />
         <a
           class={styles.link}
           href="https://play.google.com/store/apps/details?id=org.godotengine.astrorush"
@@ -35,7 +40,7 @@ const App: Component = () => {
         <article class={styles.advert}>
           <video class={styles.video} autoplay muted loop>
             <source src={gameplay} type="video/mp4" />
-            Your browser does not support the video tag.
+            <img src={gameplay_still} class={styles.gameplayStill} alt="gameplay" />
           </video>
           <img src={phone} class={styles.phone} alt="phone" />
         </article>
